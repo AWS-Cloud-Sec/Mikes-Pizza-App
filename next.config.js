5c3be550e06cdbbe14c3dac3dd37d4230ff8d7d6
MiguelAntonioMaciasMeza <miguel6021857@gmail.com>
2025-04-24 18:11:34 -0700

    Updated next.config
    Added s3 bucket host name for nextJS to allow us to display images from there.

--- next.config.js
+++ next.config.js
@@ -4,19 +4,23 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'example.com',
+        protocol: "https",
+        hostname: "example.com",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      }
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "mikes-pizza-assets.s3.us-east-1.amazonaws.com",
+      },
     ],
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
