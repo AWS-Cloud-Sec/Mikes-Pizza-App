17422f4f6149a1a4545cb44dc4b9dc6b686bdd32
Haothai1 <Hao2003thai@gmail.com>
2025-04-29 09:34:25 -0700

    updated the CheckoutForm component to receive the clientSecret prop

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -86,7 +86,7 @@ export default function CheckoutPage() {
           {/* Payment form section */}
           <div className="bg-white p-6 rounded-lg shadow">
             <Elements stripe={stripePromise} options={{ clientSecret }}>
-              <CheckoutForm />
+              <CheckoutForm clientSecret={clientSecret} />
             </Elements>
           </div>
           
