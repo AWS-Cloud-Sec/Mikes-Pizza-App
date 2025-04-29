bc62b3fb413f6ee61e9cbfab571903927185ec44
MiguelAntonioMaciasMeza <miguel6021857@gmail.com>
2025-04-28 22:19:36 -0700

    Created awsExports.js
    Added everything needed for cognito to work with AWS-Amplify/Auth module

---
+++ app/awsExports.js
@@ -0,0 +1,33 @@
+import { Amplify } from "aws-amplify";
+
+const awsExports = {
+  Auth: {
+    Cognito: {
+      userPoolId: "us-east-1_WbPOW8DjF",
+      userPoolClientId: "2kkh4u94kop7vu9o09ijscue6o",
+      loginWith: {
+        oauth: {
+          domain: "https://us-east-1iiajpyalk.auth.us-east-1.amazoncognito.com",
+          scopes: [
+            "openid",
+            "email",
+            "phone",
+            "profile",
+            "aws.cognito.signin.user.admin",
+          ],
+          redirectSignIn: ["http://localhost:3000/"],
+          redirectSignOut: ["http://localhost:3000/"],
+          responseType: "code",
+        },
+        username: false,
+        email: true,
+        phone: true,
+        name: true,
+        family_name: true,
+        address: true,
+      },
+    },
+  },
+};
+
+export default awsExports;
