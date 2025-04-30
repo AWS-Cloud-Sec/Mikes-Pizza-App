ed2baa5872ce018c9a65ec55f95f3a719f31a97a
MiguelAntonioMaciasMeza <miguel6021857@gmail.com>
2025-04-29 20:59:21 -0700

    Editted useAuth
    Now has ahandleChallenge function that submits user's userattributes if they are missing. Also sends back a password forthe challenge response

--- app/hooks/useAuth.tsx
+++ app/hooks/useAuth.tsx
@@ -4,22 +4,28 @@ import { signIn, confirmSignIn } from "aws-amplify/auth";
 
 function useAuth() {
   const [userChallenge, setUserChallenge] = useState<any>("");
-  const [requireNewPassword, setRequireNewPassword] = useState(false);
+  const [requireNextStep, setRequireNextStep] = useState(false);
   const [error, setError] = useState("");
+  const [requestParameters, setRequestParameters] = useState<string[]>([]);
 
-  async function login(email: string, password: string) {
+  async function login(formData: { [name: string]: string }) {
     try {
       const response = await signIn({
-        username: email,
-        password: password,
+        username: formData.username,
+        password: formData.password,
       });
       if (
         "nextStep" in response &&
         response.nextStep.signInStep ===
           "CONFIRM_SIGN_IN_WITH_NEW_PASSWORD_REQUIRED"
       ) {
+        if (response.nextStep?.missingAttributes) {
+          //Add in the request parameters if needed
+          setRequestParameters(response.nextStep?.missingAttributes);
+        }
+
         setError("You require a new password");
-        setRequireNewPassword(true);
+        setRequireNextStep(true);
         setUserChallenge(response);
       }
       return response;
@@ -28,29 +34,37 @@ function useAuth() {
     }
   }
 
-  async function validateNewPassword(
-    newPassword: string,
-    confirmPassword: string
+  async function handleChallenge(
+    payload: { [name: string]: string },
+    password: string
   ) {
     try {
-      //First check if the passwords match
-      if (newPassword != confirmPassword) {
-        setError("Passwords do not match! Try again");
-        return;
-      }
+      console.log(payload);
 
       //If there isn't a user being challenged that means there isn't a session
       if (!userChallenge) {
         throw new Error("No User Session");
       } else {
-        const result = await confirmSignIn({ challengeResponse: newPassword });
+        const result = await confirmSignIn({
+          challengeResponse: password,
+          options: {
+            userAttributes: payload,
+          },
+        });
         console.log("Result = ", result);
+        return result;
       }
     } catch (error: any) {
       setError(error.message || "Failed reseting a new password");
     }
   }
-  return { login, validateNewPassword, requireNewPassword, error };
+  return {
+    login,
+    handleChallenge,
+    requireNextStep,
+    requestParameters,
+    error,
+  };
 }
 
 export { useAuth };
