894470f3b2163b0db4541f7d5d2e5d6b4553b7ed
MiguelAntonioMaciasMeza <miguel6021857@gmail.com>
2025-05-03 03:35:21 -0700

    Edited stripe config
    Instead ofpointing to our locally ran next sever,it will low point to our lambda function online

--- app/config/stripe.ts
+++ app/config/stripe.ts
@@ -1,22 +1,28 @@
 import { loadStripe } from "@stripe/stripe-js";
-
+import { fetchAuthSession } from "@aws-amplify/auth";
 // Initialize Stripe with  public key
 export const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
 );
-console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 // Proceed to Checkout to for payment
 export const createPaymentIntent = async (amount: number) => {
+  const session = await fetchAuthSession();
+  const idToken = session.tokens?.idToken?.toString();
+  console.log(idToken);
   try {
     // Send the order amount to our Stripe server
-    const response = await fetch("/api/create-payment-intent", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ amount }),
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_GATEWAY_URL}create-payment-intent`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${idToken}`,
+        },
+        body: JSON.stringify({ amount }),
+      }
+    );
 
     // Throw an error, if something went wrong with payment
     if (!response.ok) {
